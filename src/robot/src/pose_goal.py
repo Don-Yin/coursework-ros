5dabbee4f0d35fc3df7b1b30ef6f674ce7a50082
Don Yin <Don_Yin@outlook.com>
2023-07-01 18:38:32 +0800

    Update pose_goal.py

--- src/robot/src/pose_goal.py
+++ src/robot/src/pose_goal.py
@@ -6,65 +6,33 @@ import moveit_msgs
 import moveit_commander
 import geometry_msgs
 import numpy as np
+import time
 
 
 def move_end_effector():
-    # initial some node information so ROSCore knows what we are
+    # initialize the node
     moveit_commander.roscpp_initialize(sys.argv)
     rospy.init_node("move_end_effector", anonymous=True)
 
-    # start some moveit specific classes for the planning -- this is robot specific
+    # start MoveIt classes
     robot = moveit_commander.RobotCommander()
     group_name = "needle_group"
     move_group = moveit_commander.MoveGroupCommander(group_name)
 
-    # We need to specify the end effector
+    # specify the end effector
     move_group.set_end_effector_link("needle")
 
-    # Print out the current end effector position
+    # get and print the current end effector position
     current_pose = move_group.get_current_pose().pose
-    print("Current end effector position: ", current_pose)
+    print("Current end effector position:", current_pose)
 
-    # Generate random values for each axis
-    rand_x = current_pose.position.x + np.random.uniform(-0.01, 0.01)
-    rand_y = current_pose.position.y + np.random.uniform(-0.01, 0.01)
-    rand_z = current_pose.position.z + np.random.uniform(-0.01, 0.01)
+    # start listening and printing every second
+    while not rospy.is_shutdown():
+        current_pose = move_group.get_current_pose().pose
+        print("Current end effector position:", current_pose)
+        time.sleep(1)
 
-    # set up a pose goal -- right now just hard coded
-    pose_goal = geometry_msgs.msg.Pose()
-    pose_goal.position.x = rand_x
-    pose_goal.position.y = rand_y
-    pose_goal.position.z = rand_z
-
-    # set the pose goal
-    move_group.set_pose_target(pose_goal)
-
-    # plan the motion and execute
-    plan = move_group.go(wait=True)
-
-    while not plan:
-        print("Planning failed, trying again")
-
-        # Generate random values for each axis
-        rand_x = current_pose.position.x + np.random.uniform(-0.01, 0.01)
-        rand_y = current_pose.position.y + np.random.uniform(-0.01, 0.01)
-        rand_z = current_pose.position.z + np.random.uniform(-0.01, 0.01)
-
-        # set up a pose goal -- right now just hard coded
-        pose_goal = geometry_msgs.msg.Pose()
-        pose_goal.position.x = rand_x
-        pose_goal.position.y = rand_y
-        pose_goal.position.z = rand_z
-
-        # set the pose goal
-        move_group.set_pose_target(pose_goal)
-        plan = move_group.go(wait=True)
-
-    # stop the program from exiting until the motion is finished
-    move_group.stop()
-    move_group.clear_pose_targets()
-
-    # shut down moveit_commander
+    # shutdown MoveIt
     moveit_commander.roscpp_shutdown()
 
 
