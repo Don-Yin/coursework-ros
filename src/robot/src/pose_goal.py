d368dafd5644fdf46810c6943b4ebf270fc54710
Don Yin <Don_Yin@outlook.com>
2023-07-01 17:38:04 +0800

    Update pose_goal.py

--- src/robot/src/pose_goal.py
+++ src/robot/src/pose_goal.py
@@ -14,15 +14,15 @@ def pose_goal():
 
     # start some moveit specific classes for the planning -- this is robot specific
     robot = moveit_commander.RobotCommander()
-    group_name = "arm_group"
+    group_name = "needle_group"
     move_group = moveit_commander.MoveGroupCommander(group_name)
 
     # set up a pose goal -- right now just hard coded
     pose_goal = geometry_msgs.msg.Pose()
     pose_goal.orientation.w = 1.0
-    pose_goal.position.x = 0.4
-    pose_goal.position.y = 0.1
-    pose_goal.position.z = 0.4
+    pose_goal.position.x = 208.596
+    pose_goal.position.y = 133.917
+    pose_goal.position.z = 115.449
 
     # solve for the pose goal
     move_group.set_pose_target(pose_goal)
@@ -38,3 +38,4 @@ if __name__ == "__main__":
         pose_goal()
     except rospy.ROSInterruptException:
         pass
+
