1f1944c3a2c60c49f79961295bd514f734146847
Don Yin <Don_Yin@outlook.com>
2023-07-02 14:06:15 +0800

    update

--- src/robot/src/move.py
+++ src/robot/src/move.py
@@ -2,46 +2,82 @@
 
 import rospy
 import sys
-import moveit_msgs
 import moveit_commander
-import geometry_msgs
 
 
-def move_end_effector():
-    # initial some node information so ROSCore knows what we are
-    moveit_commander.roscpp_initialize(sys.argv)
-    rospy.init_node("move_end_effector", anonymous=True)
+class CommandArm:
+    def __init__(self):
+        moveit_commander.roscpp_initialize(sys.argv)
+        rospy.init_node("move_end_effector", anonymous=True)
 
-    # start some moveit specific classes for the planning -- this is robot specific
-    robot = moveit_commander.RobotCommander()
-    group_name = "needle_group"
-    move_group = moveit_commander.MoveGroupCommander(group_name)
+    def move_random(self):
+        """
+        The robot variable can be used for:
+        1. Get the Robot's Kinematic Model
+        2. Get the Current State of the Robot
+        3. Check Joint Limits
+        4. Get the Robot's End Effectors
+        5. Set the Robot's Start State
+        6. Check Collision
+        """
+        robot = moveit_commander.RobotCommander()
+        group_name = "arm_group"
+        move_group = moveit_commander.MoveGroupCommander(group_name)
+        move_group.set_end_effector_link("needle")
+        move_group.set_random_target()
+        plan_success = move_group.go(wait=True)
 
-    # We need to specify the end effector
-    move_group.set_end_effector_link("needle")
+        while not plan_success:
+            print("Planning failed, trying again")
+            move_group.set_random_target()
+            plan_success = move_group.go(wait=True)
 
-    # Set target position
-    target_position = [10.0, 10.0, 10.0]
-    move_group.set_position_target(target_position)
+        move_group.stop()
+        move_group.clear_pose_targets()
 
-    # plan the motion and execute
-    plan = move_group.go(wait=True)
+    def command_needle(self, pose: str):
+        robot = moveit_commander.RobotCommander()
+        group_name = "needle_group"
+        move_group = moveit_commander.MoveGroupCommander(group_name)
 
-    # If the plan fails, alert the user
-    while not plan:
-        print("Planning failed, trying again")
-        plan = move_group.go(wait=True)
+        move_group.set_named_target(pose)
+        plan_success = move_group.go(wait=True)
 
-    # stop the program from exiting until the motion is finished
-    move_group.stop()
-    move_group.clear_pose_targets()
+        while not plan_success:
+            print("Planning failed, trying again")
+            move_group.set_named_target(pose)
+            plan_success = move_group.go(wait=True)
 
-    # shut down moveit_commander
-    moveit_commander.roscpp_shutdown()
+        move_group.stop()
+        move_group.clear_pose_targets()
+
+    def command_arm(self, pose: str = "Home"):
+        robot = moveit_commander.RobotCommander()
+        group_name = "arm_group"
+        move_group = moveit_commander.MoveGroupCommander(group_name)
+
+        move_group.set_named_target(pose)
+        plan_success = move_group.go(wait=True)
+
+        while not plan_success:
+            print("Planning failed, trying again")
+            move_group.set_named_target(pose)
+            plan_success = move_group.go(wait=True)
+
+        move_group.stop()
+        move_group.clear_pose_targets()
+
+    def on_finish(self):
+        moveit_commander.roscpp_shutdown()
 
 
 if __name__ == "__main__":
     try:
-        move_end_effector()
+        command_arm = CommandArm()
+        command_arm.move_random()
+        command_arm.command_arm("Home")
+        command_arm.command_needle("Retracted")
+        command_arm.command_needle("Extended")
+        command_arm.on_finish()
     except rospy.ROSInterruptException:
         pass
--- src/robot/src/random_number.py
+++ src/robot/src/random_number.py
@@ -6,25 +6,15 @@ from std_msgs.msg import Int32
 
 
 def random_publisher():
-    # Create a new publisher. We specify the topic name, then type of message which is integer
     rospy.init_node("random_number_publisher", anonymous=True)
     pub = rospy.Publisher("random_number", Int32, queue_size=10)
-
-    # Set the loop rate
     rate = rospy.Rate(10)  # 10hz
 
     while not rospy.is_shutdown():
-        # Generate a random number
         random_number = Int32()
         random_number.data = random.randint(0, 100)
-
-        # Publish the message
         pub.publish(random_number)
-
-        # Log info
         rospy.loginfo("I published a random number: %s", random_number.data)
-
-        # Sleep for the remaining time to hit our 10hz publish rate
         rate.sleep()
 
 
