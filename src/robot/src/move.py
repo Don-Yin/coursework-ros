bbe9b542fd8514f7a9f21307b0a5d521e9adb464
Don Yin <Don_Yin@outlook.com>
2023-07-02 21:27:16 +0800

    Update move.py

--- src/robot/src/move.py
+++ src/robot/src/move.py
@@ -49,9 +49,8 @@ class CommandArm:
         group_name = "arm_group"
         move_group = moveit_commander.MoveGroupCommander(group_name)
         move_group.set_end_effector_link("sphere")
-
-        # Set the planning time to a higher value
         move_group.set_planning_time(10)
+
         move_group.set_position_target(coordinates)
         plan_success = move_group.go(wait=True)
 
@@ -67,15 +66,15 @@ class CommandArm:
         group_name = "arm_group"
         move_group = moveit_commander.MoveGroupCommander(group_name)
         move_group.set_end_effector_link("sphere")
+        move_group.set_planning_time(10)
 
         # Calculate the direction vector from entry to target
         direction = target - entry
-        # direction = direction / np.linalg.norm(direction)
+        direction = direction / np.linalg.norm(direction)
 
         # Calculate quaternion from the 'entry' vector to the 'direction' vector
         quaternion = quaternion_from_two_vectors(entry, direction)
 
-        # Set the pose target
         pose_target = geometry_msgs.msg.Pose()
         pose_target.position.x = entry[0]
         pose_target.position.y = entry[1]
@@ -84,15 +83,13 @@ class CommandArm:
         pose_target.orientation.y = quaternion[1]
         pose_target.orientation.z = quaternion[2]
         pose_target.orientation.w = quaternion[3]
-        move_group.set_pose_target(pose_target)
 
         # Planning and executing the motion
-        plan_success = move_group.go(wait=True)
+        move_group.go(pose_target, wait=True)
 
-        while not plan_success:
-            print("Planning failed, trying again")
-            move_group.set_pose_target(pose_target)
-            plan_success = move_group.go(wait=True)
+        # # while not plan_success:
+        # if not plan_success:
+        #     print("Planning failed, trying again")
 
         move_group.stop()
         move_group.clear_pose_targets()
