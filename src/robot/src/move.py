bb232e2782614a087e0a2d21207df9f9a8eb5009
Don Yin <Don_Yin@outlook.com>
2023-07-02 22:00:26 +0800

    Update move.py

--- src/robot/src/move.py
+++ src/robot/src/move.py
@@ -66,7 +66,7 @@ class CommandArm:
         group_name = "arm_group"
         move_group = moveit_commander.MoveGroupCommander(group_name)
         move_group.set_end_effector_link("sphere")
-        move_group.set_planner_id("RRTConnect")
+        # move_group.set_planner_id("RRTConnect")
 
         # Calculate the direction vector from entry to target
         direction = target - entry
