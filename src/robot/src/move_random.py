9c9da5a115d46f01ea677b529b828e8eaa5d3869
Don Yin <Don_Yin@outlook.com>
2023-07-02 13:49:38 +0800

    Update move_random.py

--- src/robot/src/move_random.py
+++ src/robot/src/move_random.py
@@ -38,8 +38,39 @@ def move_end_effector():
     moveit_commander.roscpp_shutdown()
 
 
+def command_needle(pose: str):
+    # initial some node information so ROSCore knows what we are
+    moveit_commander.roscpp_initialize(sys.argv)
+    rospy.init_node("move_end_effector", anonymous=True)
+
+    # start some moveit specific classes for the planning -- this is robot specific
+    robot = moveit_commander.RobotCommander()
+    group_name = "needle_group"
+    move_group = moveit_commander.MoveGroupCommander(group_name)
+
+    # Set the target to the predefined pose
+    move_group.set_named_target(pose)
+
+    # plan the motion and execute
+    plan_success = move_group.go(wait=True)
+
+    # If the plan fails, alert the user
+    while not plan_success:
+        print("Planning failed, trying again")
+        move_group.set_named_target(pose)
+        plan_success = move_group.go(wait=True)
+
+    # stop the program from exiting until the motion is finished
+    move_group.stop()
+    move_group.clear_pose_targets()
+
+    # shut down moveit_commander
+    moveit_commander.roscpp_shutdown()
+
+
 if __name__ == "__main__":
     try:
         move_end_effector()
+        command_needle("Retracted")
     except rospy.ROSInterruptException:
         pass
