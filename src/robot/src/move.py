ba5ed153522eb90c025e8ae172b62d64d338471e
Don Yin <Don_Yin@outlook.com>
2023-07-03 16:52:54 +0800

    Update move.py

--- src/robot/src/move.py
+++ src/robot/src/move.py
@@ -90,7 +90,7 @@ class CommandArm:
         move_group.set_max_velocity_scaling_factor(1.0)
         move_group.set_max_acceleration_scaling_factor(1.0)
 
-        move_group.set_goal_position_tolerance(10)
+        move_group.set_goal_position_tolerance(0.5)
         # move_group.set_goal_orientation_tolerance(0.1)
 
         # Calculate the direction vector from entry to target
