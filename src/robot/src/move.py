77e7a48eb9dcf43fee48da97b39155d948a7f506
Don Yin <Don_Yin@outlook.com>
2023-07-03 16:05:29 +0800

    update

--- src/robot/src/move.py
+++ src/robot/src/move.py
@@ -12,7 +12,7 @@ import math
 def convert_slicer_to_ros(point):
     scale_factor = 0.08
     point = point * scale_factor
-    position_correction = np.array([-29.0, -10, 0])
+    position_correction = np.array([-29.0, -10, -0.5])
     point = point + position_correction
     return np.array([-point[0], point[1], point[2]])
 
@@ -89,7 +89,7 @@ class CommandArm:
         move_group.set_max_acceleration_scaling_factor(1.0)
 
         # move_group.set_goal_position_tolerance(0.5)
-        # move_group.set_goal_orientation_tolerance(0.2)
+        move_group.set_goal_orientation_tolerance(0.1)
 
         # Calculate the direction vector from entry to target
         direction = target - entry
